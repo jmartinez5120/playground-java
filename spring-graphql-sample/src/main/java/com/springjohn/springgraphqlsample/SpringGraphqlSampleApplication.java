96e6e03ebced83b3ee5f63b49cc48b5ad8038e7e
John Martinez <jmartinez5120@gmail.com>
2021-12-07 23:23:16 -0500

    Added sample class files from the Sprin initializer.

---
+++ spring-graphql-sample/src/main/java/com/springjohn/springgraphqlsample/SpringGraphqlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.springjohn.springgraphqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphqlSampleApplication.class, args);
+	}
+
+}
---
+++ spring-graphql-sample/src/test/java/com/springjohn/springgraphqlsample/SpringGraphqlSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjohn.springgraphqlsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
