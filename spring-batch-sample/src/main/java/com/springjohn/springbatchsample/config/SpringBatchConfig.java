6bbb20be2ab80572d12ff12aaea6c3c38f721eb6
John Martinez <jmartinez5120@gmail.com>
2021-12-20 22:21:07 -0500

    APP WORKS. HASN'T been tested yet.

--- spring-batch-sample/src/main/java/com/springjohn/springbatchsample/config/SpringBatchConfig.java
+++ spring-batch-sample/src/main/java/com/springjohn/springbatchsample/config/SpringBatchConfig.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-// Annotation need for newer versions of Spring Batch
+//EnableBatchProcessing Annotation need for newer versions of Spring Batch
 @EnableBatchProcessing
 @Configuration
 public class SpringBatchConfig {
