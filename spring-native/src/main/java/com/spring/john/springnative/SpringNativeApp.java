93723fca02355745a8b94ef879ee953ed9de85bf
johnmartinez <jmartinez5120@gmail.com>
2022-05-24 20:49:25 -0400

    Adding Spring Native dependencies.

---
+++ spring-native/src/main/java/com/spring/john/springnative/SpringNativeApp.java
@@ -0,0 +1,8 @@
+package com.spring.john.springnative;
+
+public class SpringNativeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Spring Native App up and running.");
+    }
+}
