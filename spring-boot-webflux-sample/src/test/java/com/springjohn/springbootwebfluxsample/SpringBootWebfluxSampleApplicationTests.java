8ddd49cb3656b62408d0af39cdc3229ab31deec2
jmart <jmartinez5120@gmail.com>
2023-02-18 01:41:02 -0500

    Some message.

---
+++ spring-boot-webflux-sample/src/test/java/com/springjohn/springbootwebfluxsample/SpringBootWebfluxSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjohn.springbootwebfluxsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
