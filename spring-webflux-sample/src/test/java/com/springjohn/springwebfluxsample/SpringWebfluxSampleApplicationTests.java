059d13caec7791f85fe7f4cdda0e3405fabadf76
johnmartinez <jmartinez5120@gmail.com>
2023-04-16 13:12:30 -0400

    Erasing the empty folder from Spring Webflux Udemy course sample

---
+++ spring-webflux-sample/src/test/java/com/springjohn/springwebfluxsample/SpringWebfluxSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springjohn.springwebfluxsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
