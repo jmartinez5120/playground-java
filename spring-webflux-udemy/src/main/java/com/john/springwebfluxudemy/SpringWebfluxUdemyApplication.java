211915e22b60c789a1930d4923c550ea11e8d1b1
jmart <jmartinez5120@gmail.com>
2023-04-17 22:33:18 -0400

    Initializing the project

---
+++ spring-webflux-udemy/src/main/java/com/john/springwebfluxudemy/SpringWebfluxUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.john.springwebfluxudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxUdemyApplication.class, args);
+	}
+
+}
---
+++ spring-webflux-udemy/src/test/java/com/john/springwebfluxudemy/SpringWebfluxUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.springwebfluxudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
