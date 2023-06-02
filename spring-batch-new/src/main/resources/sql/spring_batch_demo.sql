31224fe46ee7f205e6dbcd40f0f9a0fbf672b317
johnmartinez <jmartinez5120@gmail.com>
2023-06-02 05:23:55 -0400

    started new demo

---
+++ spring-batch-new/src/main/resources/sql/spring_batch_demo.sql
@@ -0,0 +1,11 @@
+create table dummy_data.spring_batch_demo
+(
+    id serial primary key ,
+    first_name varchar(50),
+    last_name varchar(50),
+    create_datetime timestamp,
+    update_datetime timestamp
+);
+
+
+
