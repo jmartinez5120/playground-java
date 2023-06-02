50c8c06008b67243f8b9c92e5935cc159391ee5a
johnmartinez <jmartinez5120@gmail.com>
2023-06-02 05:24:17 -0400

    started new demo

---
+++ spring-batch-new/src/main/java/com/john/springbatchnew/SpringBatchNewApplication.java
@@ -0,0 +1,13 @@
+package com.john.springbatchnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchNewApplication.class, args);
+    }
+
+}
