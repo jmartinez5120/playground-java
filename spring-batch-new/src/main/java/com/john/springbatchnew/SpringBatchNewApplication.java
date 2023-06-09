0822cb6eebe876b1cfbdd4e5d670cf7c59668e09
johnmartinez <jmartinez5120@gmail.com>
2023-06-09 13:37:52 -0400

    Started building the batch config jobs.

--- spring-batch-new/src/main/java/com/john/springbatchnew/SpringBatchNewApplication.java
+++ spring-batch-new/src/main/java/com/john/springbatchnew/SpringBatchNewApplication.java
@@ -1,9 +1,11 @@
 package com.john.springbatchnew;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchNewApplication {
 
     public static void main(String[] args) {
