620d071f4bcd87a4e9f778cf9192c10ad945156a
jmart <jmartinez5120@gmail.com>
2023-10-18 07:23:14 -0400

    Added a sample MongoDB application.

---
+++ spring-data-mongodb/src/main/java/com/john/springdatamongodb/ProductController.java
@@ -0,0 +1,28 @@
+package com.john.springdatamongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
+
---
+++ spring-data-mongodb/src/main/java/com/john/springdatamongodb/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.john.springdatamongodb;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
+
